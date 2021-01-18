f1618bb132aa8444f31f46e985a38c31fd3c4249
Jordan Western <ek9_fly@hotmail.co.uk>
2021-01-18 16:47:42 +0000

    initial commit

---
+++ ADOTestSync/TestSyncConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestSyncConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
